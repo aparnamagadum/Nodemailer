a5834a7d9d3b3a710117658b56d509028909d255
aparnamagadum <aparnamagadum2@gmail.com>
2024-09-11 13:05:45 +0530

    contact form

---
+++ client/contact/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+function App() {
+  const [name,setName]=useState('');
+  const [email,setEmail]=useState('');
+  const [text,setText]=useState('');
+  async function sendData(e){
+    e.preventDefault();
+    try{
+    await axios.post('http://localhost:3939/sendMail' , {name , email , text})
+    console.log("mail sent");
+    
+    }
+    catch(err){console.log(err);}
+    
+  }
+  return (
+    <>
+     <form onSubmit={sendData}>
+      <h1>Contact</h1>
+      <input type="text" name="name" placeholder='enter your name' value={name} onChange={(e)=>setName(e.target.value)}/>
+      <input type="email" name="email" placeholder='enter your mail' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <textarea rows='5' placeholder='enter your message' value={text} onChange={(e)=>setText(e.target.value)}/>
+      <button type='submit'>send Email</button>
+      <br />
+     </form>
+    </>
+  )
+}
+
+export default App
