d7537e5c880f523159a722ba0f0b506beb5cf3a1
aparnamagadum <aparnamagadum2@gmail.com>
2024-09-11 14:47:18 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"*"
 }));
 //console.log(process.env.My_USERNAME);
 //console.log(process.env.My_PASSWORD);
