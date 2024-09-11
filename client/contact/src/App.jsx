ce0056a965ceb339e86874c5471f4157d59c743e
aparnamagadum <aparnamagadum2@gmail.com>
2024-09-11 14:17:54 +0530

    deployed server

--- client/contact/src/App.jsx
+++ client/contact/src/App.jsx
@@ -7,19 +7,19 @@ function App() {
   const [name,setName]=useState('');
   const [email,setEmail]=useState('');
   const [text,setText]=useState('');
-  // async function sendData(e){
-  //   e.preventDefault();
-  //   try{
-  //   await axios.post('http://localhost:3939/sendMail' , {name , email , text})
-  //   console.log("mail sent");
+  async function sendData(e){
+    e.preventDefault();
+    try{
+    await axios.post('https://nodemailer-server-jn33.onrender.com/sendMail' , {name , email , text})
+    console.log("mail sent");
     
-  //   }
-  //   catch(err){console.log(err);}
+    }
+    catch(err){console.log(err);}
     
-  // }
+  }
   return (
     <>
-    {/* //onSubmit={sendData} */}
+      onSubmit={sendData}
      <form >
       <h1>Contact</h1>
       <input type="text" name="name" placeholder='enter your name' value={name} onChange={(e)=>setName(e.target.value)}/>
